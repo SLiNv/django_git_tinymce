5b0c6a85b3b351cfb759b572818fcccf043109ee
Victor Zhu <zzy809@gmail.com>
2017-11-09 23:52:33 -0500

    Linted

--- django_git/settings.py
+++ django_git/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles', 
+    'django.contrib.staticfiles',
     # third party apps
     'bootstrap3',
     'ckeditor',
